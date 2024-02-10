35f8d02de976242eea14a726b0cbe7b6b70d079d
mikejeter <mjeter45@yahoo.com>
2024-02-10 00:45:23 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+// Import any necessary modules or libraries here
+
+// Define your landing page function
+function landingPage() {
+  // Add your landing page logic here
+}
+
+// Call the landing page function to execute it
+landingPage();
